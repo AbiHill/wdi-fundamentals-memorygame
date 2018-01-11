5a1232b754510901a4c4b4a897b112a179b0e0d8
abi hill <abi@abis-mbp.lan>
2018-01-11 14:48:27 +0000

    created game logic unit 9

--- js/main.js
+++ js/main.js
@@ -3,24 +3,26 @@ var cards = ["queen" , "queen" , "king" , "king"];
 
 var cardsInPlay = [];
 
-var cardOne; 
-
-cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardsInPlay);
-
-var cardTwo;
-
-cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
+var checkForMatch = function(){
 
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match!")}
 
+	else if (cardsInPlay.length < 2) {alert("Draw another card!")}
+
 	else
 
-{alert("Sorry, try again!")};
+{alert("Sorry, try again!")}
+
+};
+
+var flipCard = function(cardId){
+console.log("User flipped" + " " + cards[cardId]);
+
+cardsInPlay.push(cards[cardId]);
+
+checkForMatch();
+
+};
+
+flipCard(0);
+flipCard(2);
